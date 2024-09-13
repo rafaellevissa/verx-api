775cacb4806c0790891abffcf1be3b1d39f4ca50
rafaellevissa <rafallevissa@gmail.com>
2024-09-13 13:48:47 -0300

    Add locations migration

---
+++ database/migrations/1726092658101_create_locations_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'locations'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('city', 100)
+      table.string('state', 2)
+      table.integer('farm_id').unsigned().references('id').inTable('farms').onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
